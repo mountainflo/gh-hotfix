9de2ef48de1221110c73163f64cc217672266cf6
mountainflo <florian.tobusch@gmx.de>
2024-03-31 23:09:11 +0200

    match commits from PRs and main branch

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"hash/fnv"
 	"os"
 	"os/exec"
+	"sort"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/google/go-github/v37/github"
 	"golang.org/x/oauth2"
@@ -16,6 +20,42 @@ const (
 	owner = "mountainflo"
 	repo  = "gh-hotfix" // TODO get this info from current repo
 )
+
+type commitMatch struct {
+	pr         *github.PullRequest
+	prCommit   wrappedCommit
+	mainCommit wrappedCommit
+}
+
+type wrappedCommit struct {
+	commit *github.RepositoryCommit
+}
+
+// hash creates a hash value to easily identify a commit
+// Commit SHAs can't be used as they change after rebase&merge of the PR is done
+func (c wrappedCommit) hash() uint32 {
+	v := c.commit.GetCommit()
+	h := fnv.New32a()
+	h.Write([]byte(v.GetMessage()))
+	if v.GetAuthor() != nil {
+		// For Author the date should be the same for the commit of the PR and the commit within main branch
+		// For Commiter the date will be different as rebase&merge created a commit
+		h.Write([]byte(fmt.Sprintf("%v", v.GetAuthor().GetDate())))
+	} else {
+		// problematic in case of duplicate commit messages
+		fmt.Sprintf("WARN: no commitAuthor field. hash will only be generated from commit msg")
+	}
+	return h.Sum32()
+}
+
+func (c wrappedCommit) equal(other wrappedCommit) bool {
+	a := c.commit.GetCommit()
+	b := other.commit.GetCommit()
+	return a.GetMessage() == b.GetMessage() &&
+		a.GetStats().GetDeletions() == b.GetStats().GetDeletions() &&
+		a.GetStats().GetAdditions() == b.GetStats().GetAdditions() &&
+		a.GetStats().GetTotal() == b.GetStats().GetTotal()
+}
 
 func main() {
 
@@ -62,20 +102,46 @@ func main() {
 	fmt.Printf("Creating hotfix %s based on %s.\n", hotfixName, releaseBranch)
 	fmt.Printf("Cherry-Picking commits of PRs '%s' from branch '%s'\n", pullRequests, mainBranch)
 
-	// Erstelle eine GitHub-Clientverbindung
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
+	client := createGitHubClient(token, ctx)
 
-	isMerged, err := isPRMerged(ctx, client, owner, repo, 1)
+	prs, err := getMergedPullRequests(pullRequests, client, ctx)
 	if err != nil {
-		fmt.Printf("Failed to check if PR is merged: %v\n", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("isMerged: %v\n", isMerged)
+	if len(prs) == 0 {
+		fmt.Println("Empty PR List")
+		os.Exit(1)
+	}
 
-	// Erstelle einen neuen Branch für den Hotfix
+	unmatchedPrCommits, err := collectCommitsFromPRs(prs, client, ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// fetch commits from main branch
+	mainBranchCommits, _, err := client.Repositories.ListCommits(ctx, owner, repo, &github.CommitsListOptions{
+		SHA:   mainBranch,
+		Since: getOldestPRCreationDate(prs),
+	})
+	if err != nil {
+		fmt.Printf("Failed to list commits: %v\n", err)
+		os.Exit(1)
+	}
+
+	matchingCommits, err := matchCommits(mainBranchCommits, unmatchedPrCommits)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	for _, commit := range matchingCommits {
+		fmt.Printf("mainCommitSHA: %v; prCommitSHA: %v\n", commit.mainCommit.commit.GetSHA(), commit.prCommit.commit.GetSHA())
+	}
+
+	// TODO create or checkout hotfix branch based on the release branch
 	/*_, _, err := client.Git.CreateRef(ctx, "<owner>", "<repo>", &github.Reference{
 		Ref:    github.String("refs/heads/" + hotfixName),
 		Object: &github.GitObject{SHA: github.String("<sha-of-base-branch>")},
@@ -84,6 +150,93 @@ func main() {
 		fmt.Printf("Failed to create hotfix branch: %v\n", err)
 		os.Exit(1)
 	}*/
+
+	// TODO cherry pick commits to hotfix branch
+
+	// TODO push hotfix branch
+
+	// TODO open PR for hotfix and add a nice summary of the included PRs
+}
+
+func matchCommits(mainBranchCommits []*github.RepositoryCommit, unmatchedPrCommits map[uint32]commitMatch) ([]commitMatch, error) {
+	var matchingCommits []commitMatch
+	for _, commit := range mainBranchCommits {
+		wrappedMainBranchCommit := wrappedCommit{commit: commit}
+
+		commitMatch, ok := unmatchedPrCommits[wrappedMainBranchCommit.hash()]
+
+		if ok {
+			commitMatch.mainCommit = wrappedMainBranchCommit
+			matchingCommits = append(matchingCommits, commitMatch)
+			delete(unmatchedPrCommits, wrappedMainBranchCommit.hash())
+		}
+	}
+
+	// verify unmatchedPrCommits is empty
+	if len(unmatchedPrCommits) != 0 {
+		return nil, fmt.Errorf("commits could be matched", unmatchedPrCommits)
+	}
+
+	return matchingCommits, nil
+}
+
+func collectCommitsFromPRs(prs []*github.PullRequest, client *github.Client, ctx context.Context) (map[uint32]commitMatch, error) {
+	hashToCommitMatch := make(map[uint32]commitMatch)
+	for _, pr := range prs {
+		commits, _, err := client.PullRequests.ListCommits(ctx, owner, repo, *pr.Number, nil)
+		if err != nil {
+			return nil, fmt.Errorf("can't retrieve commits for PR %s: %v", *pr.Number, err)
+		}
+		for _, c := range commits {
+			wc := wrappedCommit{commit: c}
+			hashToCommitMatch[wc.hash()] = commitMatch{
+				prCommit: wc,
+				pr:       pr,
+			}
+		}
+	}
+	return hashToCommitMatch, nil
+}
+
+func createGitHubClient(token string, ctx context.Context) *github.Client {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	return client
+}
+
+func getMergedPullRequests(pullRequests string, client *github.Client, ctx context.Context) ([]*github.PullRequest, error) {
+	var prs []*github.PullRequest
+	for _, prNum := range strings.Split(strings.ReplaceAll(pullRequests, "#", ""), ",") {
+		prInt, _ := strconv.Atoi(prNum)
+		pr, _, err := client.PullRequests.Get(ctx, owner, repo, prInt)
+		if err != nil {
+			return nil, fmt.Errorf("error for fetching PR data from GitHub: %v", err)
+		}
+		if pr.GetMerged() {
+			prs = append(prs, pr)
+		} else {
+			fmt.Printf("Non-merged PR #%v can't be added to hotfix. PR has state: %v\n", *pr.Number, *pr.State)
+		}
+	}
+	return prs, nil
+}
+
+func sortPRsByMergeDateAsc(prs []*github.PullRequest) {
+	sort.Slice(prs, func(i, j int) bool {
+		return prs[i].MergedAt.After(*(prs[j].MergedAt))
+	})
+}
+
+func getOldestPRCreationDate(prs []*github.PullRequest) time.Time {
+	oldest := time.Now()
+	for _, pr := range prs {
+		prCreationDate := *(pr.CreatedAt)
+		if oldest.After(prCreationDate) { // TODO check if it works for different time zones, too
+			oldest = prCreationDate
+		}
+	}
+	return oldest
 }
 
 func getGitHubToken() string {
@@ -92,7 +245,7 @@ func getGitHubToken() string {
 		return token
 	}
 
-	// Retrieve GITHUB_TOKEN using gh config
+	// retrieve GITHUB_TOKEN using "gh config" command
 	cmd := exec.Command("gh", "config", "get", "oauth_token", "-h", "github.com")
 	output, err := cmd.Output()
 	if err != nil {
@@ -103,15 +256,4 @@ func getGitHubToken() string {
 	token = strings.TrimSuffix(string(output), "\n")
 
 	return token
-}
-
-// isPRMerged checks if a PR is merged
-func isPRMerged(ctx context.Context, client *github.Client, owner, repo string, prNum int) (bool, error) {
-
-	pr, _, err := client.PullRequests.Get(ctx, owner, repo, prNum)
-	if err != nil {
-		return false, err
-	}
-
-	return pr.GetMerged(), nil
 }
