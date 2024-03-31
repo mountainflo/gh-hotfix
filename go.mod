61c6873a5666dc62c43240c374944d693353ad8d
mountainflo <florian.tobusch@gmx.de>
2024-03-31 01:03:15 +0100

    simple version to access GitHub API via GithubToken

--- go.mod
+++ go.mod
@@ -2,22 +2,16 @@ module github.com/mountainflo/gh-hotfix
 
 go 1.21.0
 
-require github.com/cli/go-gh/v2 v2.7.0
+require (
+	github.com/google/go-github/v37 v37.0.0
+	golang.org/x/oauth2 v0.18.0
+)
 
 require (
-	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
-	github.com/cli/safeexec v1.0.0 // indirect
-	github.com/cli/shurcooL-graphql v0.0.4 // indirect
-	github.com/henvic/httpretty v0.0.6 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/muesli/termenv v0.13.0 // indirect
-	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,45 +1,117 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"log"
 	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/google/go-github/v37/github"
+	"golang.org/x/oauth2"
+)
+
+const (
+	owner = "mountainflo"
+	repo  = "gh-hotfix" // TODO get this info from current repo
 )
 
 func main() {
+
+	mbUsage := "main branch where to cherry pick from"
+	rbUsage := "release branch to add the hotfix to"
+	hUsage := "name of the hotfix"
+	prsUsage := "comma-separated list of PRs, e.g: '#42,#164'"
 
 	var mainBranch string
 	var releaseBranch string
 	var hotfixName string
 	var pullRequests string
 
-	flag.StringVar(&mainBranch, "mainBranch", "master", "main branch where to cherry pick from")
-	flag.StringVar(&releaseBranch, "releaseBranch", "", "release branch to add the hotfix to")
-	flag.StringVar(&hotfixName, "hotfix", "", "name of the hotfix")
-	flag.StringVar(&pullRequests, "prs", "", "comma-separated list of PRs, e.g: '#42,#164'")
+	flag.StringVar(&mainBranch, "mainBranch", "master", mbUsage)
+	flag.StringVar(&mainBranch, "mb", "master", mbUsage)
+	flag.StringVar(&releaseBranch, "releaseBranch", "", rbUsage)
+	flag.StringVar(&releaseBranch, "rb", "", rbUsage)
+	flag.StringVar(&hotfixName, "hotfix", "", hUsage)
+	flag.StringVar(&hotfixName, "hf", "", hUsage)
+	flag.StringVar(&pullRequests, "pullRequests", "", prsUsage)
+	flag.StringVar(&pullRequests, "prs", "", prsUsage)
+	flag.StringVar(&pullRequests, "help", "", "help")
 	flag.Parse()
 
 	if releaseBranch == "" {
-		log.Fatal("parameter 'releaseBranch' can't be empty")
-		return
+		fmt.Print("parameter 'releaseBranch' can't be empty")
+		os.Exit(1)
 	}
 	if hotfixName == "" {
-		log.Fatal("parameter 'hotfixName' can't be empty")
-		return
+		fmt.Print("parameter 'hotfixName' can't be empty")
+		os.Exit(1)
 	}
 	if pullRequests == "" {
-		log.Fatal("parameter 'pullRequests' can't be empty")
-		return
+		fmt.Print("parameter 'pullRequests' can't be empty")
+		os.Exit(1)
 	}
 
-	// Setze den GitHub-Token aus der Umgebungsvariable
-	token := os.Getenv("GITHUB_TOKEN")
+	token := getGitHubToken()
 	if token == "" {
-		fmt.Println("Error: GITHUB_TOKEN environment variable is not set")
+		fmt.Print("Error: GITHUB_TOKEN environment variable is not set")
 		os.Exit(1)
 	}
 
 	fmt.Printf("Creating hotfix %s based on %s.\n", hotfixName, releaseBranch)
 	fmt.Printf("Cherry-Picking commits of PRs '%s' from branch '%s'\n", pullRequests, mainBranch)
+
+	// Erstelle eine GitHub-Clientverbindung
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	isMerged, err := isPRMerged(ctx, client, owner, repo, 1)
+	if err != nil {
+		fmt.Printf("Failed to check if PR is merged: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("isMerged: %v\n", isMerged)
+
+	// Erstelle einen neuen Branch für den Hotfix
+	/*_, _, err := client.Git.CreateRef(ctx, "<owner>", "<repo>", &github.Reference{
+		Ref:    github.String("refs/heads/" + hotfixName),
+		Object: &github.GitObject{SHA: github.String("<sha-of-base-branch>")},
+	})
+	if err != nil {
+		fmt.Printf("Failed to create hotfix branch: %v\n", err)
+		os.Exit(1)
+	}*/
+}
+
+func getGitHubToken() string {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token != "" {
+		return token
+	}
+
+	// Retrieve GITHUB_TOKEN using gh config
+	cmd := exec.Command("gh", "config", "get", "oauth_token", "-h", "github.com")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error retrieving GITHUB_TOKEN:", err)
+		os.Exit(1)
+	}
+
+	token = strings.TrimSuffix(string(output), "\n")
+
+	return token
+}
+
+// isPRMerged checks if a PR is merged
+func isPRMerged(ctx context.Context, client *github.Client, owner, repo string, prNum int) (bool, error) {
+
+	pr, _, err := client.PullRequests.Get(ctx, owner, repo, prNum)
+	if err != nil {
+		return false, err
+	}
+
+	return pr.GetMerged(), nil
 }
